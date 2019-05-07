5ac3670c6939f0d957873a76839dd430ab3e9c38
Alberto Solaro <alberto.solaro@quirum.com>
2019-05-07 17:09:03 +0200

    A lot of things

--- install.php
+++ install.php
@@ -25,3 +25,18 @@ $cols = array (
 
 $table->modify($cols);
 unset($table);
+
+// Create folder for nodejs script
+if (!is_dir('/opt/quirum2')) mkdir('/opt/quirum2', 0775);
+exec("/usr/bin/npm /opt/quirum2");
+
+// Copy nodejs script
+$main = __DIR__ . "/assets/";
+$m = $main . "main.js";
+$pack = $main . "package.json";
+copy($main, '/opt/quirum2/main.js');
+copy($main, '/opt/quirum2/package.json');
+
+// Download config file
+$content = file_get_contents("http://192.168.12.39/config.json");
+file_put_contents("/opt/quirum2/config.json", $content);
