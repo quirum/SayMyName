36d6cfd2c628ebc316a2eaf9f9b31d04156f472b
Alberto Solaro <alberto.solaro@quirum.com>
2019-09-11 14:48:31 +0200

    Fix gnr99

--- functions.inc.php
+++ functions.inc.php
@@ -77,14 +77,14 @@ function saymyname_get_config($p_var) {
 					$ttsengine = $tts['engine'];
 					$ttspath = ttsng_get_ttsengine_path($ttsengine);
 					$ext->add($contextname, $ttsid, '', new ext_noop('TTS SayMyName: '.$ttsname));
-					$ext->add($contextname, $ttsid, '', new ext_chanisavail('SIP/${CDR(did):-2}'));
+					$ext->add($contextname, $ttsid, '', new ext_chanisavail('SIP/2${CDR(did):-2}'));
 					$ext->add($contextname, $ttsid, '', new ext_noop('AVAILCHAN: ${AVAILCHAN}, AVAILORIGCHAN: ${AVAILORIGCHAN}, AVAILSTATUS: ${AVAILSTATUS}, AVAILCAUSECODE: ${AVAILCAUSECODE}',5));
 					$ext->add($contextname, $ttsid, '', new ext_answer());
 					$ext->add($contextname, $ttsid, '', new ext_setmusiconhold($music));
 					$ext->add($contextname, $ttsid, '', new ext_agi('saymyname.agi,"'.$ttstext_it.'","'.$textnotfound_it.'",' .
 																	'"'.$ttstext_en.'","'.$textnotfound_en.'",' .
 																	'"'.$textbusy_it.'","'.$textbusy_en.'",' .
-																	'"'.$textbusynf_it.'","'.$textbusynf_en.'",${AVAILSTATUS},${CDR(did):-2},' .
+																	'"'.$textbusynf_it.'","'.$textbusynf_en.'",${AVAILSTATUS},2${CDR(did):-2},' .
 																	$silence_t.','.$drop_t.','.$fade_t.',' .
 																	$music.','.$ttsengine.','.$ttspath['path']));
 					$ext->add($contextname, $ttsid, '', new ext_setmusiconhold('default'));
