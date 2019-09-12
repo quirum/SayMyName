d55625fe49cff31f0be390ac00cf33f59d8356a9
Alberto Solaro <alberto.solaro@quirum.com>
2019-09-12 15:17:42 +0200

    fix for future update

--- assets/install.sh
+++ assets/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/html/admin/modules/saymyname/assets/ && /usr/bin/npm install
+cd /var/www/html/admin/modules/saymynamene/assets/ && /usr/bin/npm install
 su asterisk -c "cd /var/www/html/admin/modules/saymynamene/assets/ && /usr/bin/npm install -g"
